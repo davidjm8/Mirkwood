6ba7e2eb37b3272a9fee9e2b2855a4067aedde92
Trainerpl022 <Trainerpl022@SLFRPL022PCFORM.ensino.atec.local>
2016-11-04 11:41:34 +0000

    collision 04/11/2016 do david

---
+++ src/main/java/artefactos/Gold.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package artefactos;
+
+/**
+ *
+ * @author UserPL022Pc01
+ */
+public class Gold {
+    
+}
